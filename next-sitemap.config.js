75373ccdc6ca70c6dfaaccd805d73e32a7c11dab
Omar Ibrahim <omaribrahim@cs.uchicago.edu>
2023-01-19 17:51:19 -0600

    add support for dynamic project urls; integrate with firebase

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,5 +2,7 @@
 module.exports = {
     siteUrl: process.env.SITE_URL || 'https://omar-ibrahim.com',
     generateRobotsTxt: true, // (optional)
+  generateIndexSitemap: false,
+  sitemapSize: 1000,
     // ...other options
 }
