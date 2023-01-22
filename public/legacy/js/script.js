97e2035200c1b22a7addcb8284b36330e7d14923
Omar Ibrahim <omaribrahim@cs.uchicago.edu>
2023-01-21 20:47:14 -0600

    fix faceshot

--- public/legacy/js/script.js
+++ public/legacy/js/script.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
 
+    // Prespective on hover
+    document.querySelectorAll('.prsp').forEach(function(card) {
+        card.addEventListener('mousemove', prespectiveOnHover);
+    });
+
     // Brings sub-menus to Bootstrap 4 (by Gerhard Gotz on StackOverFlow)
     $('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
         if (!$(this).next().hasClass('show')) {
@@ -99,7 +104,8 @@ $(document).ready(function() {
     }
 
     // Particle Js
-    particlesJS("particles-js", { "particles": { "number": { "value": 260, "density": { "enable": true, "value_area": 800 } }, "color": { "value": "#ffffff" }, "shape": { "type": "circle", "stroke": { "width": 0, "color": "#000000" }, "polygon": { "nb_sides": 5 }, "image": { "src": "img/github.svg", "width": 100, "height": 100 } }, "opacity": { "value": 1, "random": true, "anim": { "enable": true, "speed": 1, "opacity_min": 0, "sync": false } }, "size": { "value": 3, "random": true, "anim": { "enable": false, "speed": 4, "size_min": 0.3, "sync": false } }, "line_linked": { "enable": false, "distance": 150, "color": "#ffffff", "opacity": 0.4, "width": 1 }, "move": { "enable": true, "speed": 1, "direction": "none", "random": true, "straight": false, "out_mode": "out", "bounce": false, "attract": { "enable": false, "rotateX": 600, "rotateY": 600 } } }, "interactivity": { "detect_on": "canvas", "events": { "onhover": { "enable": true, "mode": "bubble" }, "onclick": { "enable": true, "mode": "repulse" }, "resize": true }, "modes": { "grab": { "distance": 400, "line_linked": { "opacity": 1 } }, "bubble": { "distance": 250, "size": 0, "duration": 2, "opacity": 0, "speed": 3 }, "repulse": { "distance": 400, "duration": 0.4 }, "push": { "particles_nb": 4 }, "remove": { "particles_nb": 2 } } }, "retina_detect": true });
+    // particlesJS("particles-js", { "particles": { "number": { "value": 260, "density": { "enable": true, "value_area": 800 } }, "color": { "value": "#ffffff" }, "shape": { "type": "circle", "stroke": { "width": 0, "color": "#000000" }, "polygon": { "nb_sides": 5 }, "image": { "src": "img/github.svg", "width": 100, "height": 100 } }, "opacity": { "value": 1, "random": true, "anim": { "enable": true, "speed": 1, "opacity_min": 0, "sync": false } }, "size": { "value": 3, "random": true, "anim": { "enable": false, "speed": 4, "size_min": 0.3, "sync": false } }, "line_linked": { "enable": false, "distance": 150, "color": "#ffffff", "opacity": 0.4, "width": 1 }, "move": { "enable": true, "speed": 1, "direction": "none", "random": true, "straight": false, "out_mode": "out", "bounce": false, "attract": { "enable": false, "rotateX": 600, "rotateY": 600 } } }, "interactivity": { "detect_on": "canvas", "events": { "onhover": { "enable": true, "mode": "bubble" }, "onclick": { "enable": true, "mode": "repulse" }, "resize": true }, "modes": { "grab": { "distance": 400, "line_linked": { "opacity": 1 } }, "bubble": { "distance": 250, "size": 0, "duration": 2, "opacity": 0, "speed": 3 }, "repulse": { "distance": 400, "duration": 0.4 }, "push": { "particles_nb": 4 }, "remove": { "particles_nb": 2 } } }, "retina_detect": true });
+    // particlesJS("particles-js", {"particles":{"number":{"value":80,"density":{"enable":true,"value_area":800}},"color":{"value":"#ffffff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"repulse"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});
 
     // Activate Carousel
     $('.carousel').carousel({
@@ -834,5 +840,28 @@ function selectNews(index) {
     clearInterval(window.progressInterval);
     showNews(0, index);
 };
+
+function prespectiveOnHover(mouseEvent) {
+    let $this = $(this);
+    let $parent = $this.parent();
+    let $parentWidth = $parent.width();
+    let $parentHeight = $parent.height();
+    let $parentOffset = $parent.offset();
+    let $parentOffsetLeft = $parentOffset.left;
+    let $parentOffsetTop = $parentOffset.top;
+    let $parentCenterX = $parentOffsetLeft + $parentWidth / 2;
+    let $parentCenterY = $parentOffsetTop + $parentHeight / 2;
+    let $mouseX = mouseEvent.pageX;
+    let $mouseY = mouseEvent.pageY;
+    let $mouseXFromCenter = $mouseX - $parentCenterX;
+    let $mouseYFromCenter = $mouseY - $parentCenterY;
+    let $perspective = 1000;
+    let $rotateY = $mouseXFromCenter / $perspective;
+    let $rotateX = -$mouseYFromCenter / $perspective;
+    let $translateZ = Math.abs($mouseXFromCenter) / 100 + Math.abs($mouseYFromCenter) / 100;
+    $parent.css({
+        "transform": "perspective(" + $perspective + "px) rotateX(" + $rotateX + "deg) rotateY(" + $rotateY + "deg) translateZ(" + $translateZ + "px)"
+    });
+}
 
 window.news = selectNews;
